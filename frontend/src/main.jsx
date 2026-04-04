4e929593d63045f73cb163f0acb828eefe5c61d2
Mannloser <patelman542@gmail.com>
2026-04-05 00:50:27 +0530

    updated frontend and backend changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,9 +5,7 @@ import "./index.css";
 import { HashRouter } from "react-router-dom"; // ✅ ADD THIS
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <HashRouter>   {/* ✅ WRAP APP */}
+  <React.StrictMode> {/* ✅ WRAP APP */}
       <App />
-    </HashRouter>
   </React.StrictMode>
 );
